0d58b26e823790629c3e3c88f9693d8a2ea27dc6
sevenler <johnnyxyzw@gmail.com>
2014-08-15 12:08:17 +0800

    显示网络表情

--- src/com/jemoji/http/TestCast3.java
+++ src/com/jemoji/http/TestCast3.java
@@ -1,12 +1,18 @@
 
 package com.jemoji.http;
 
+import java.io.File;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.concurrent.CountDownLatch;
 
 import android.test.AndroidTestCase;
 
 import com.jemoji.image.RequestManager;
 import com.jemoji.utils.LOG;
+import com.upyun.api.Uploader;
+import com.upyun.api.utils.UpYunException;
+import com.upyun.api.utils.UpYunUtils;
 
 public class TestCast3 extends AndroidTestCase {
 	// //////////////////////////////////////////////////////////////////////////////////////////
@@ -48,7 +54,7 @@ public class TestCast3 extends AndroidTestCase {
 		checkWrong();
 	}
 
-	public void testCase() throws Throwable {
+	public void htestCase() throws Throwable {
 		RequestManager.init(getContext());
 		URLs.changeEnvoriment(URLs.ENVORIMENT_FORMAL);
 		
@@ -65,15 +71,57 @@ public class TestCast3 extends AndroidTestCase {
 		});
 		finishResponse();*/
 		
-		
-		preRequest();
-		GKHttpInterface.pushMessage("18511557126", "00000000000000000000", new GKJsonResponseHandler() {
-			@Override
-			public void onResponse(int code, Object json, Throwable error) {
-				checkResponse(code, json, error);
-			}
-		});
-		finishResponse();
+//		preRequest();
+//		GKHttpInterface.pushMessage("18511557126", "00000000000000000000", new GKJsonResponseHandler() {
+//			@Override
+//			public void onResponse(int code, Object json, Throwable error) {
+//				checkResponse(code, json, error);
+//			}
+//		});
+//		finishResponse();
 	}
 
+	
+	private static final String TEST_API_KEY = "h2u0v4S4BRUtwQphIj6sBy+JVMo="; // 测试使用的表单api验证密钥
+	private static final String BUCKET = "emoji"; // 存储空间
+	private static final long EXPIRATION = System.currentTimeMillis() / 1000 + 1000 * 5 * 10; // 过期时间，必须大于当前时间
+	
+	public void testCase0() throws Throwable {
+		RequestManager.init(getContext());
+		URLs.changeEnvoriment(URLs.ENVORIMENT_FORMAL);
+		
+		
+		File path = new File("/sdcard/Emoji_Image/");
+		File[] list = path.listFiles();
+		
+		
+		List<String> files = new LinkedList<String>();
+		for(File f : list){
+			files.add(f.getAbsolutePath());
+		}
+		
+		for(String file : files){
+			String result = uploadFile(file);
+			System.out.println(String.format("hello:%s", result));
+		}
+	}
+	
+	private String getFullName(String name){
+		return String.format("http://emoji.b0.upaiyun.com/%s", name);
+	}
+	
+	private String uploadFile(String file) throws UpYunException{
+		
+		String result = file.substring(file.lastIndexOf(File.separator) + 1, file.length());
+		
+		String path = File.separator + "test" + File.separator + result;
+		// 取得base64编码后的policy
+		String policy = UpYunUtils.makePolicy(path, EXPIRATION, BUCKET);
+		// 根据表单api签名密钥对policy进行签名
+		// 通常我们建议这一步在用户自己的服务器上进行，并通过http请求取得签名后的结果。
+		String signature = UpYunUtils.signature(policy + "&" + TEST_API_KEY);
+		// 上传文件到对应的bucket中去。
+		String string = Uploader.upload(policy, signature, BUCKET, file);
+		return string;
+	}
 }
