1f5f3168b077c0561f5b86c7d80662324cf78c3c
sevenler <johnnyxyzw@gmail.com>
2014-08-20 11:20:42 +0800

    修复消息crash bug

--- src/com/jemoji/FileUploader.java
+++ src/com/jemoji/FileUploader.java
@@ -15,7 +15,6 @@ import com.upyun.api.utils.UpYunUtils;
 public  class FileUploader {
 	private static final String TEST_API_KEY = "h2u0v4S4BRUtwQphIj6sBy+JVMo="; // 测试使用的表单api验证密钥
 	private static final String BUCKET = "emoji"; // 存储空间
-	private static final long EXPIRATION = System.currentTimeMillis() / 1000 + 1000 * 5 * 10; // 过期时间，必须大于当前时间
 	
 	public static interface UploadCallBack{
 		public void onResult();
@@ -27,6 +26,7 @@ public  class FileUploader {
 			// 设置服务器上保存文件的目录和文件名，如果服务器上同目录下已经有同名文件会被自动覆盖的。
 			String path = File.separator + "test" + File.separator + result;
 
+			long EXPIRATION = System.currentTimeMillis() / 1000 + 1000 * 5 * 10;
 			// 取得base64编码后的policy
 			String policy = UpYunUtils.makePolicy(path, EXPIRATION, BUCKET);
 
